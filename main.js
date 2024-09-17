8644f6e397c3488f83d2f60a765ea393f6bdb04d
Mrithul P <149092428+MrithulP@users.noreply.github.com>
2024-09-17 20:17:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,22 @@ function toggleNavbar() {
   const sidebar = document.getElementById('sidebar');
   sidebar.classList.toggle('shrink');
 }
-// Show the appropriate slide
+// Change slide
 function showSlide(slideId) {
-  // Hide all slides
+
   const slides = document.querySelectorAll('.slide');
   slides.forEach(slide => slide.classList.remove('active'));
 
-  // Show the selected slide
+
   const activeSlide = document.getElementById(`${slideId}-slide`);
   activeSlide.classList.add('active');
 
-  // Remove 'active' class from all sidebar menu items
+
   const menuItems = document.querySelectorAll('.menu li');
   menuItems.forEach(item => item.classList.remove('active'));
+  
 
-  // Add 'active' class to the clicked menu item
+
   const activeMenuItem = document.getElementById(`${slideId}-btn`);
   activeMenuItem.classList.add('active');
 }
